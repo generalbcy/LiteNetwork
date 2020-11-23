3d7f5025b0e422ab0b61fa770c5767c504fd49cc
KSExtrez <extrez10@gmail.com>
2020-11-22 22:41:58 -0600

    Add comments

--- src/LiteNetwork.Common/ILiteConnectionToken.cs
+++ src/LiteNetwork.Common/ILiteConnectionToken.cs
@@ -5,10 +5,19 @@ namespace LiteNetwork.Common
 {
     public interface ILiteConnectionToken
     {
+        /// <summary>
+        /// Gets the connection attached to the current token.
+        /// </summary>
         ILiteConnection Connection { get; }
 
+        /// <summary>
+        /// Gets the socket connection.
+        /// </summary>
         Socket Socket { get; }
 
+        /// <summary>
+        /// Gets the data token.
+        /// </summary>
         LiteDataToken DataToken { get; }
     }
 }
